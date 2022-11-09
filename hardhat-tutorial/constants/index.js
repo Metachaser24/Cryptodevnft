d96dfee191b05cabf377deb19920163b73b9bb3b
Metachaser24 <elomukoro@gmail.com>
2022-11-09 13:43:47 -0600

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xDC78BA2220A6d0D0431c67bCAa2678BD6Cfd608f";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
